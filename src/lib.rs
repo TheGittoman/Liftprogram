61ab0421312cb21f81bc2097a99f176188d3609a
Gittoman <tomvar98@gmail.com>
2024-02-09 12:16:33 +0200

    add lift functionality added

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use chrono::{DateTime, NaiveDate};
+use chrono::{DateTime, Month, Months, NaiveDate};
+use std::io::Write;
+use std::ops::Add;
+use std::{env, error, io};
 use std::{error::Error, fs, str::FromStr};
 
 pub fn search(lifts: &Vec<Lift>, search_term: &str, sflag: char) -> Result<(), &'static str> {
@@ -16,6 +19,11 @@ pub fn search(lifts: &Vec<Lift>, search_term: &str, sflag: char) -> Result<(), &
                 if lift.location == search_term || lift.name == search_term {
                     println!("{}", lift.to_string());
                 }
+            }
+        }
+        'a' => {
+            for lift in lifts {
+                println!("{}", lift.to_string());
             }
         }
         _ => return Err("No valid flag profided for searc"),
@@ -60,6 +68,69 @@ fn parse_line(line: &String) -> Lift {
         chrono::NaiveDate::from_str(values[2]).unwrap(),
         chrono::NaiveDate::from_str(values[3]).unwrap(),
     )
+}
+
+// add lift in database / lift.exe add <lift-id> -> location maintenance date
+fn add_lift(arg: &String, lifts: &mut Vec<Lift>) -> Result<(), &'static str> {
+    println!("Type a new lift instance in following pattern.");
+    println!("location maintenance-date");
+    loop {
+        // get input to create the new lift
+        let mut input = String::new();
+        print!("input: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("error reading the line");
+        let input: Vec<&str> = input.split_whitespace().collect();
+        // check if there is enough items in the input
+        if input.len() != 2 {
+            println!("{:?}", input);
+            println!("Not enough inputs");
+            continue;
+        }
+        // assign the new naivedate to a variable and check for errors
+        let maintenance = match NaiveDate::from_str(input[1]) {
+            Ok(value) => value,
+            Err(err) => {
+                println!("Error: {err}");
+                continue;
+            }
+        };
+        // assign variable with current maintenance date + 3 months
+        let next_maintenance = maintenance.checked_add_months(Months::new(3)).unwrap();
+        let lift = Lift::build(
+            arg.to_string(),
+            input[0].to_string(),
+            maintenance,
+            next_maintenance,
+        );
+        lifts.push(lift);
+        break;
+    }
+    io::stdout().flush().unwrap();
+    Ok(())
+}
+
+fn parse_args(args: &Vec<String>, lifts: &mut Vec<Lift>) -> Result<(), &'static str> {
+    if args.len() < 3 && args[1] != "list".to_string() {
+        return Err("Too few arguments");
+    }
+    match args[1].as_str() {
+        "add" => add_lift(&args[2], lifts).unwrap(),
+        "list" => {
+            if args.len() < 3 {
+                search(&lifts, "all", 'a').unwrap();
+            } else {
+                search(&lifts, &args[2], 'n').unwrap();
+            }
+        }
+        "delete" => println!("delete"),
+        "fix" => println!("fix"),
+        "edit" => println!("edit"),
+        _ => return Err("Command not found"),
+    }
+    Ok(())
 }
 
 pub struct Config {
@@ -123,10 +194,19 @@ impl Lift {
 }
 
 pub fn run() {
-    let config = Config::build("option".to_string(), "hissit.txt".to_string());
-    let mut lifts = read_file(&config);
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let config = Config::build("option".to_string(), "hissit.txt".to_string());
+        let mut lifts = read_file(&config);
+        parse_args(&args, &mut lifts).unwrap();
+        for lift in &lifts {
+            lift.print();
+        }
+        write_file(&config, &lifts).unwrap();
+    } else {
+        println!("Not enough arguments provided!");
+    }
     // let lift = Lift::new();
     // lifts.push(lift);
     // write_file(&config, &lifts).unwrap();
-    search(&lifts, "2025-1-1", 't').unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@
 // add edit delete fix options
 // ability to search lifts that have maintenance in certain time in future (1month etc.)
 
-use chrono::{Date, DateTime, NaiveDate};
-use database;
-use std::{env, fs, panic::Location, str::FromStr};
+use std::{env, io};
+
+// use database;
 
 fn main() {
     // let args: Vec<String> = env::args().collect();
@@ -20,3 +20,38 @@ fn main() {
     // println!("{}", date.format("%d-%y-%m"));
     database::run();
 }
+
+pub struct Config {
+    option: String,
+    option2: String,
+}
+
+impl Config {
+    pub fn build(option: String, option2: String) -> Config {
+        Config {
+            option: option,
+            option2: option2,
+        }
+    }
+}
+
+fn add() {
+    //
+}
+
+/*
+app.exe LIST -> lists all locations and how many lifts there are, also shows
+    how many lifts have maintenance coming in months time
+    - LIST + <location> prints all the names (IDs) and info about lifts in this
+      location
+app.exe MAINTENANCE 1m/w/d -> lists all lifts that have maintenance coming in
+    specified timeframe
+app.exe ADD <lift_name> <location> -> adds a new lift with current day as the
+    maintenance date, and next_maintenance date 3 months in future.
+app.exe DELETE <lift_name> - deletes the lift from the list
+    will show the lift that will be deleted and ask for confirmation
+app.exe EDIT <lift_name> ->
+
+app.exe add <lift>
+app.exe maintenance 3m
+*/
